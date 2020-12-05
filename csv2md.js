2f0eebf854e0f44ff4760984f94b32720a75a140
amutake <amutake.s@gmail.com>
2020-12-06 02:06:19 +0900

    Refactor

--- csv2md.js
+++ csv2md.js
@@ -3,26 +3,34 @@ const parse = require('csv-parse/lib/sync');
 const table = require('markdown-table');
 const stringWidth = require('string-width');
 
-const csv = fs.readFileSync(`${__dirname}/data.csv`);
-const data = parse(csv, {
-  columns: true,
-});
-data.sort((a, b) => {
-  if (
-    a.published_at > b.published_at ||
-    (a.published_at === b.published_at && a.title < b.title)
-  ) {
-    return -1;
-  }
-  if (
-    a.published_at < b.published_at ||
-    (a.published_at === b.published_at && a.title > b.title)
-  ) {
-    return 1;
-  }
-  return 0;
-});
-const md = () => {
+const parseCsv = (path) => {
+  const csv = fs.readFileSync(path);
+  const data = parse(csv, {
+    columns: true,
+  });
+  return data;
+};
+
+const sort = (data) => {
+  data.sort((a, b) => {
+    if (
+      a.published_at > b.published_at ||
+      (a.published_at === b.published_at && a.title < b.title)
+    ) {
+      return -1;
+    }
+    if (
+      a.published_at < b.published_at ||
+      (a.published_at === b.published_at && a.title > b.title)
+    ) {
+      return 1;
+    }
+    return 0;
+  });
+  return data;
+};
+
+const makeTable = (data) => {
   const a = [['title', 'author', 'published_at']];
   data.forEach((e) => {
     let title = e.title;
@@ -37,4 +45,5 @@ const md = () => {
   });
   return table(a, { stringLength: stringWidth });
 };
-console.log(md());
+
+console.log(makeTable(sort(parseCsv(`${__dirname}/data.csv`))));
